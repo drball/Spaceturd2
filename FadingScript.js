a6ac99b0926886faa937d47b2c2bc9565848d053
david <david@ongoingworlds.com>
2015-06-26 09:12:42 +0100

    Added fading script to fade screen in/out

---
+++ FadingScript.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+var fadeTexture : Texture2D;
+var fadeSpeed = 0.2;
+var drawDepth = -1000;
+
+private var alpha = 1.0; 
+private var fadeDir = -1;
+
+function OnGUI(){
+
+	alpha += fadeDir * fadeSpeed * Time.deltaTime;  
+	alpha = Mathf.Clamp01(alpha);   
+
+	GUI.color.a = alpha;
+
+	GUI.depth = drawDepth;
+
+	GUI.DrawTexture(Rect(0, 0, Screen.width, Screen.height), fadeTexture);
+}
+
+public function BeginFade(direction : int){
+	fadeDir = direction;
+	return (fadeSpeed);
+}
+
+function OnLevelWasLoaded (level : int) {
+	Debug.Log("level"+level+" was loaded");
+	BeginFade(-1);
+}
---
+++ MovingPlatform.js
@@ -0,0 +1,66 @@
+﻿#pragma strict
+
+private var Xpos : float;
+private var Ypos : float;
+var countdown: int;
+var max : boolean;
+var Vert : boolean;
+var Horiz : boolean;
+var maxAmountY : int;
+var maxAmountX : int;
+var step : float;
+var delayTime : int;
+ 
+function Start () {
+     Xpos = transform.position.x;
+     Ypos = transform.position.y;
+}
+
+
+function Update () {
+
+
+	if (countdown > 0) {
+		countdown--;
+	} else {
+	
+		//SET THE MAX
+		if(Vert){ //Vertical
+			if(transform.position.y >= Ypos + maxAmountY){
+				max = true;
+				countdown = delayTime;
+			} else if(transform.position.y <= Ypos){
+				max = false;
+				countdown = delayTime;
+			}
+		}
+
+		if(Horiz){//Horizontal
+			if(transform.position.x >= Xpos + maxAmountX){
+				max = true;
+				countdown = delayTime;
+			} else if(transform.position.x <= Xpos){
+				max = false;
+				countdown = delayTime;
+			}
+		}
+
+		//MOVING THE PLATFORM
+		if(Vert){ // Vertical movement
+			if(!max){
+				transform.position.y += (step * Time.deltaTime);
+			} else {
+				transform.position.y -= (step * Time.deltaTime);
+			} 
+		}
+
+		if(Horiz){ //Horizontal movement
+			if(!max){
+				transform.position.x += (step * Time.deltaTime);
+			} else {
+				transform.position.x -= (step * Time.deltaTime);
+			} 
+		}
+	}
+}
+
