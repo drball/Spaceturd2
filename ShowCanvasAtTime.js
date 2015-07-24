5a05ef52660f5b9e2e04d69fe447dd19bbcc3dd2
david <david@ongoingworlds.com>
2015-07-24 08:31:21 +0100

    Made the delay actually work when paused

--- ShowCanvasAtTime.js
+++ ShowCanvasAtTime.js
@@ -2,7 +2,9 @@
 
 //--for showing a canvas (maybe instructional text) which can be dismissed by pressing anything
 
-public var amtOfSeconds : float = 1;
+public var amtOfSeconds : float = 0.5;
+public var secondsOnScreen : int = 1; //--how many seconds before we can click this off
+private var endTime : int; 
 
 private var gameController : GameControllerScript;
 
@@ -17,13 +19,14 @@ function Start () {
 
 function Show () {
 	gameObject.SetActive(true);
+	endTime = secondsOnScreen + Time.time;
 	gameController.PauseGame(true);
 	Debug.Log("pause time = "+Time.time);
 }
 
 function Update () {
 
-	if(Input.anyKey && (Time.time >= 1))
+	if(Input.anyKey && (Time.realtimeSinceStartup >= endTime))
 	{
 		//--make sure this has been on screen at least 1 second before skipping
 		Debug.Log("dismiss canvas");
@@ -32,3 +35,5 @@ function Update () {
 
 	}
 }
+
+
