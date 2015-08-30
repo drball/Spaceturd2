d08e99ce7c2323cd86180eb14c99083af93a9de6
david <david@ongoingworlds.com>
2015-08-30 12:32:44 +0100

    Messing about

--- ArrowScript.js
+++ ArrowScript.js
@@ -2,27 +2,37 @@
 
 var tex : Texture2D;    // Texture to be rotated
 var target : Transform; // GameObject to point to
-var pivot : Vector2;    // Where to place the center of the texture
+private var pivot : Vector2 = Vector2(Screen.width/2, Screen.height/2);    // Where to place the center of the texture
 var mcamera: Camera;
+private var size: float = 10f;
 
 private var rect : Rect;
   
 function Start() {
-	rect = new Rect(pivot.x - tex.width * 0.5f, pivot.y - tex.height * 0.5f, tex.width, tex.height);
-	mcamera = GetComponent.<Camera>();
+	rect = new Rect(pivot.x - size * 0.5f, pivot.y - size * 0.5f, 10f, 10f);
+//	mcamera = GetComponent.<Camera>();
+}
+
+function Update() {
+	Debug.DrawLine(transform.position, target.transform.position, Color.red);
 }
  
   
  function OnGUI() {
+ 
+ 	
+ 
      if (Event.current.type == EventType.Repaint) {
          var dir = mcamera.WorldToScreenPoint(target.position);
          dir.y = Screen.height - dir.y;
          dir = dir - pivot;  
-         var angle = Mathf.Atan2(dir.y, dir.x)  * Mathf.Rad2Deg;
+         var angle = (Mathf.Atan2(dir.y, dir.x)  * Mathf.Rad2Deg) + 5f;
  
-         var matrixBackup = GUI.matrix;
+//         var matrixBackup = GUI.matrix;
          GUIUtility.RotateAroundPivot(angle, pivot);
          GUI.DrawTexture(rect, tex);
-         GUI.matrix = matrixBackup;
+//         GUI.matrix = matrixBackup;
      }
+     
+     GUI.DrawTexture(Rect(10,10,100,100), tex);
  }
---
+++ PointerToEnemy.js
@@ -0,0 +1,35 @@
+﻿#pragma strict
+
+var target : GameObject;
+var myCamera : Camera;
+var directionArrow : Texture;
+var mySprite : GameObject;
+
+function Start () {
+	
+}
+
+function Update () {
+	if (target != null) {
+		Debug.DrawLine(transform.position, target.transform.position, Color.red);
+	}
+	
+//	
+//	var viewPos : Vector3 = myCamera.WorldToScreenPoint(target.transform.position);
+//                       
+//	var angle : float = Mathf.Atan2(viewPos.x - 240, viewPos.y - 160) * Mathf.Rad2Deg;
+//	       
+//	if (viewPos.z < 0)
+//	{
+//	    if (viewPos.x > 240)
+//	        angle = 270;
+//	    else
+//	        angle = 90;
+//	}  
+//	       
+//	GUIUtility.RotateAroundPivot(angle, Vector2(240, 160));
+	//GUI.DrawTexture(Rect(80, 0, 320, 320), directionArrow);
+	mySprite.transform.LookAt(target.transform);
+}
+
+
